f7203773fc27c1a2ed427f79382818704acd7bfe
Andrew Molis <andrewmolis@gmail.com>
2021-10-27 20:17:14 -0700

    Fixed bug in graph

--- src/main/java/Dijkstra.java
+++ src/main/java/Dijkstra.java
@@ -9,7 +9,7 @@ public class Dijkstra {
         graph.addEdge(0, 1, 4);
         graph.addEdge(0, 2, 3);
         graph.addEdge(1, 0, 4);
-        graph.addEdge(1, 3, 3);
+        graph.addEdge(1, 3, 2);
         graph.addEdge(1, 2, 1);
         graph.addEdge(2, 1, 1);
         graph.addEdge(2, 3, 4);
