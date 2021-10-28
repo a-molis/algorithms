6391d0f77b9d8b98c62a3a0b21743a568d0c62bd
Andrew Molis <andrewmolis@gmail.com>
2021-10-27 20:05:50 -0700

    Removed unused getters and setters

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -7,28 +7,4 @@ public class Edge {
         this.value = value;
         this.weight = weight;
     }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public Edge getNext() {
-        return next;
-    }
-
-    public void setNext(Edge next) {
-        this.next = next;
-    }
 }
